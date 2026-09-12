ee61b6bc6659ca69ff993a8c16e0487e64a5da28
Daniel <dtwum113@gmail.com>
2026-09-12 14:18:22 +0000

    Files for establishing database connection

---
+++ backend/app/core/config.py
@@ -0,0 +1,34 @@
+from pathlib import Path  #Imports a class that allow safe work in a folder directory
+
+from pydantic import AliasChoices, Field         # The imported classes create a settings class that values from the .env file 
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+PROJECT_ROOT = Path(__file__).resolve().parents[3]
+
+#This class reads values from the .env file
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "FocusFlow API"
+    API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = "DEFAULT_SECRET_KEY_FOR_DEV_REPLACE_ME"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 11520  # 8 days
+
+    DB_USER: str = Field(default="root", validation_alias=AliasChoices("DB_USER", "USER"))
+    DB_PASSWORD: str = Field(default="password", validation_alias=AliasChoices("DB_PASSWORD", "PASSWORD"))
+    DB_HOST: str = Field(default="localhost", validation_alias=AliasChoices("DB_HOST", "HOST"))
+    DB_PORT: str = Field(default="3306", validation_alias=AliasChoices("DB_PORT", "PORT"))
+    DB_NAME: str = Field(default="Focus_Flow_AI", validation_alias=AliasChoices("DB_NAME", "DB"))
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"mysql+pymysql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"  #The URL generated used by the session layer to access the database
+
+    model_config = SettingsConfigDict(
+        env_file=[BASE_DIR / ".env", PROJECT_ROOT / ".env"],
+        env_ignore_empty=True,
+        extra="ignore",
+        case_sensitive=False,
+    )
+
+
+settings = Settings()
