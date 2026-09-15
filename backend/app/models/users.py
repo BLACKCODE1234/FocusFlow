f02a8c6b11c6b32011afce70647996bc776b5f50
Daniel <dtwum113@gmail.com>
2026-09-15 04:25:35 +0000

    Created a model that stores user info when signing up

---
+++ backend/app/models/users.py
@@ -0,0 +1,17 @@
+import enum
+from sqlalchemy import Column, Integer, String, Boolean, Enum, DateTime, ForeignKey, Text
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from app.database.session import Base
+
+
+
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(255), unique=True, index=True, nullable=False)
+    hashed_password = Column(String(255), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
