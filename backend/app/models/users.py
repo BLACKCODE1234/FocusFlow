86ad81cb12f8f8dfda0f934e7b2ca45644b7d4dc
Daniel <dtwum113@gmail.com>
2026-09-19 01:25:20 +0000

    Added column to User model to improve user verification

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -13,5 +13,8 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(255), unique=True, index=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
+    name = Column(String(100), nullable=False)
+    timezone =Column(String(50), nullable=True)
+    is_verified = Column(Boolean ,default=False, nullable =False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
