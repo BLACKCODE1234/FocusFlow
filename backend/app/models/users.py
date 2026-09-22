d35d8dfebd0bc166f06e97b9dedb7e9d33279161
Daniel <dtwum113@gmail.com>
2026-09-22 11:11:07 +0000

    Updated the user model added first name and last name

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -11,9 +11,10 @@ class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
     email = Column(String(255), unique=True, index=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
-    name = Column(String(100), nullable=False)
     timezone =Column(String(50), nullable=True)
     is_verified = Column(Boolean ,default=False, nullable =False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,16 +1,25 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr,model_validator
 from typing import Optional
 
 class UserCreate(BaseModel):
+    first_name:str
+    last_name:str
     email: EmailStr
     password: str
-    name: str
+    confirm_password:str
     timezone: Optional[str] = None   # frontend sends this silently, no form field
+
+    @model_validator(mode="after")
+    def passwords_match(self):
+        if self.password != self.confirm_password:
+            raise ValueError("Passwords do not match")
+        return self
 
 class UserOut(BaseModel):
     id: int
+    first_name: str
+    last_name: str
     email: EmailStr
-    name: str
     is_verified: bool
     class Config:
         from_attributes = True
